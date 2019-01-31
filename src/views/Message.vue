58c5cc68b70e43c2202eb1834975eb079febcc0f
liujianfengde <15698350250@163.com>
2019-01-31 15:51:30 +0800

    first commit

---
+++ src/views/Message.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="(message,index) in messages" :key="message.id">
+        <router-link :to="`/home/message/detail/${message.id}`">{{message.title}}</router-link>
+        <button @click="pushShow(message.id)">push查看</button>
+        <button @click="replaceShow(message.id)">replace查看</button>
+      </li>
+    </ul>
+    <hr>
+    <button @click="$router.back()">回退</button>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default{
+  data(){
+    return {
+      messages:[]
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      const message=[
+        {
+          id:1,
+          title:'message001'
+        },
+        {
+          id:2,
+          title:'message002'
+        },
+        {
+          id:3,
+          title:'message003'
+        },
+      ]
+      this.messages=message
+    },1000)
+  },
+  methods:{
+    pushShow(id){
+      this.$router.push(`/home/message/detail/${id}`)
+    },
+    replaceShow(id){
+      this.$router.replace(`/home/message/detail/${id}`)
+    },
+  }
+}
+</script>
+<style>
+
+</style>
---
+++ src/views/MessageDetail.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <p>ID:{{$route.params.id}}</p>
+    <ul>
+      <li>id: {{messageDetail.id}}</li>
+      <li>title: {{messageDetail.title}}</li>
+      <li>content: {{messageDetail.content}}</li>
+    </ul>
+  </div>
+</template>
+<script>
+
+export default{
+  data(){
+    return{
+      messageDetail:{}
+    }
+  },
+  mounted(){
+    setTimeout(()=>{
+      const allMessageDetails=[
+        {
+          id:1,
+          title:'message001',
+          content:'message001 content...'
+        },
+        {
+          id:2,
+          title:'message002',
+          content:'message002 content...'
+        },
+        {
+          id:3,
+          title:'message003',
+          content:'message003 content...'
+        },
+      ]
+      this.allMessageDetails=allMessageDetails
+      const id=this.$route.params.id*1
+      this.messageDetail=allMessageDetails.find(detail => detail.id===id)
+    },1000)
+  },
+  watch:{
+    $route:function (value) {
+      const id=this.$route.params.id*1
+      this.messageDetail=this.allMessageDetails.find(detail => detail.id===id)
+    }
+  }
+}
+</script>
+<style>
+
+</style>
